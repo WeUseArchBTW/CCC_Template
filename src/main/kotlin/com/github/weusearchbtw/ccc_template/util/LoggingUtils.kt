800d7bc3b78c3fd67fe196e2d701aad15235b5bc
SIMULATAN <simulatan2319@gmail.com>
2024-10-25 08:31:50 +0200

    LoggingUtils: use time in millis to optimize performance

--- src/main/kotlin/com/github/weusearchbtw/ccc_template/util/LoggingUtils.kt
+++ src/main/kotlin/com/github/weusearchbtw/ccc_template/util/LoggingUtils.kt
@@ -1,19 +1,18 @@
 package com.github.weusearchbtw.ccc_template.util
 
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 import kotlin.time.Duration
 
 fun pln(vararg arguments: Any) = println(arguments.joinToString())
 
-class ThrottledLogger(private val interval: Duration) {
-	private var lastLoggedAt = Instant.DISTANT_PAST
+class ThrottledLogger(interval: Duration) {
+	private val interval = interval.inWholeMilliseconds
+	private var lastLoggedAt: Long = 0
 	private val lock = ReentrantLock()
 
 	fun log(vararg arguments: Any) {
-		val now = Clock.System.now()
+		val now = System.currentTimeMillis()
 		lock.withLock {
 			if (now - lastLoggedAt < interval) {
 				return
