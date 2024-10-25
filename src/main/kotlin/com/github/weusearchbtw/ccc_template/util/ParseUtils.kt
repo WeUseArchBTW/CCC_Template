8fd2304bdfb259c8e33c81eacfd82c345236e434
SIMULATAN <simulatan2319@gmail.com>
2024-10-25 08:32:24 +0200

    ParseUtils: make window a List

--- src/main/kotlin/com/github/weusearchbtw/ccc_template/util/ParseUtils.kt
+++ src/main/kotlin/com/github/weusearchbtw/ccc_template/util/ParseUtils.kt
@@ -4,10 +4,10 @@ package com.github.weusearchbtw.ccc_template.util
  * Runs this[0] times
  * Windows by [parseWindowSize]
  */
-fun List<String>.firstTimesWindowedBy(parseWindowSize: (String) -> Int = { it.toInt() }, block: (Collection<String>) -> Any)
+fun List<String>.firstTimesWindowedBy(parseWindowSize: (String) -> Int = { it.toInt() }, block: (List<String>) -> Any)
 	= skip(1).timesWindowedBy(times = first().toInt(), parseWindowSize = parseWindowSize, block = block)
 
-fun List<String>.timesWindowedBy(times: Int, parseWindowSize: (String) -> Int, block: (Collection<String>) -> Any): String {
+fun List<String>.timesWindowedBy(times: Int, parseWindowSize: (String) -> Int, block: (List<String>) -> Any): String {
 	var index = 0
 	return (0..<times).joinToString(separator = "\n") { no ->
 		val windowSize = parseWindowSize(this[index++])
